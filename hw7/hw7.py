b50039c36c06206cb7b31d048bf14a767f9ae872
Frank Kuo <frankkuo@protonmail.com>
2021-03-22 02:11:45 +0800

    finish hw7

---
+++ hw7/hw7.py
@@ -0,0 +1,82 @@
+from numpy import *
+from vpython import *
+
+epsilon = 8.854E-12
+N = 101
+h = 1E-2/(N-1)
+L, d= 4E-3,1E-3
+V0 = 200
+
+def solve_laplacian(u, u_cond, h, Niter=5000):
+    V = array(u)
+    for i in range(Niter):
+        V[u_cond] = u[u_cond]
+        # V[1:-1, 1:-1] = 0 # replace this 0 by your Laplacian Solver
+        V[1:-1, 1:-1] = (V[0:-2, 1:-1] + V[2:, 1:-1] + V[1:-1, 0:-2] + V[1:-1, 2:])/4
+    return V
+
+def get_field(V, h):
+    Ex, Ey = gradient(V)
+    Ex, Ey = -Ex/h, -Ey/h
+    return Ex, Ey
+
+u = zeros([N, N])
+u[int(N/2)-int(L/h/2.0):int(N/2)+int(L/h/2.0), int(N/2) - int(d/h/2.0)] = -V0/2
+u[int(N/2)-int(L/h/2.0):int(N/2)+int(L/h/2.0), int(N/2) + int(d/h/2.0)] = V0/2
+u_cond = not_equal(u, 0)
+
+
+V = solve_laplacian(u, u_cond, h)
+
+
+scene = canvas(title='non-ideal capacitor', height=1000, width=1000, center = vec(N*h/2, N*h/2, 0))
+scene.lights = []
+scene.ambient=color.gray(0.99)
+box(pos = vec(N*h/2 , N*h/2 - d/2 - h , 0), length = L, height = h/5, width = h)
+box(pos = vec(N*h/2 , N*h/2 + d/2 - h , 0), length = L, height = h/5, width = h)
+for i in range(N):
+    for j in range(N):
+        point = box(pos=vec(i*h, j*h, 0), length = h, height= h, width = h/10, color=vec((V[i,j]+100)/200,(100-V[i,j])/200,0.0) )
+
+Ex, Ey = get_field(V, h)
+
+for i in range(0, N):
+    for j in range(0, N):
+        ar = arrow(pos = vec( i*h, j*h, h/10), axis =vec (Ex[i,j]/2E9, Ey[i,j]/2E9, 0), shaftwidth = h/6.0, color=color.black)
+
+#find Q, find C_nonideal = Q/(delta V)
+Q = 0
+# [0, 0] is in bottom left, first index is in horizontal
+plate_left = int(N/2) - int(L/h/2) - 5
+plate_right = int(N/2) + int(L/h/2) + 5
+plate_bottom = int(N/2) + int(d/h/2) - 5
+plate_top = int(N/2) + int(d/h/2) + 5
+# plate_left = int(N*(1/4))
+# plate_right = int(N*(3/4))
+# plate_bottom = int(N*(2/4))
+# plate_top = int(N*(3/4))
+
+
+
+for i in range(plate_left, plate_right+1):
+    # horizontal side
+    box(pos=vec(i*h, plate_top*h, h/10), length = h, height= h, width = h/10, color=color.cyan)
+    box(pos=vec(i*h, plate_bottom*h, h/10), length = h, height= h, width = h/10, color=color.cyan)
+    Q += h*(Ey[i, plate_top])
+    Q += h*(-Ey[i, plate_bottom])
+
+for i in range(plate_bottom, plate_top+1):
+    # vertical side
+    box(pos=vec(plate_left*h, i*h, h/10), length = h, height= h, width = h/10, color=color.cyan)
+    box(pos=vec(plate_right*h, i*h, h/10), length = h, height= h, width = h/10, color=color.cyan)
+    Q += h*(Ex[plate_right, i])
+    Q += h*(-Ex[plate_left, i])
+
+Q *= epsilon
+C_nonideal = Q/V0
+
+#Compare C_nonideal to C_ideal
+C_ideal = epsilon*L/d
+print(f"Ideal     Capacitance: {C_ideal}")
+print(f"Non-ideal Capacitance: {C_nonideal}")
+print(f"Non-ideal Difference:  {C_nonideal/C_ideal-1:.2%}")
